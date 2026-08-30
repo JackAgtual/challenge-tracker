78cfb67be725be32cd6c63d85298836cc0c5aeea
Jack Agtual <jaagtual@gmail.com>
2026-08-30 16:56:46 -0700

    Creating api client for nextjs app

---
+++ web/lib/api-client.ts
@@ -0,0 +1,21 @@
+import createClient from "openapi-fetch";
+import type { paths } from "@/types/api";
+import { auth0 } from "./auht0";
+
+export const client = createClient<paths>({
+  baseUrl: process.env.API_BASE_URL || "http://localhost:8080",
+});
+
+client.use({
+  async onRequest({ request }) {
+    console.log("client middleware");
+    const session = await auth0.getSession();
+    if (session?.tokenSet.accessToken) {
+      request.headers.set(
+        "Authorization",
+        `Bearer ${session.tokenSet.accessToken}`
+      );
+    }
+    return request;
+  },
+});
