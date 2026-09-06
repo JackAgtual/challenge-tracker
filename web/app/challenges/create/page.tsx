c4471955ed9fae8ab62e6909159a4b4c45f15342
Jack Agtual <jaagtual@gmail.com>
2026-09-06 13:30:12 -0700

    Auth check when creating challenge

--- web/app/challenges/create/page.tsx
+++ web/app/challenges/create/page.tsx
@@ -1,83 +1,13 @@
-"use client";
+import { getValidSession } from "@/lib/auth-utils";
+import CreateChallengeForm from "./components/CreateChallengeForm";
 
-import { createChallenge } from "@/actions/create-challenge";
-import { Button } from "@/components/ui/button";
-import { Field, FieldError, FieldLabel } from "@/components/ui/field";
-import { Input } from "@/components/ui/input";
-import {
-  createChallengeFormSchema,
-  TCreateChallengeFormSchema,
-} from "@/types/form-types";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { redirect } from "next/navigation";
-import { Controller, useForm } from "react-hook-form";
-
-export default function Page() {
-  const { control, handleSubmit, setError, formState } =
-    useForm<TCreateChallengeFormSchema>({
-      resolver: zodResolver(createChallengeFormSchema),
-      defaultValues: {
-        name: "",
-        durationDays: 0,
-      },
-    });
-
-  const onSubmit = async (formData: TCreateChallengeFormSchema) => {
-    const res = await createChallenge(formData);
-    if (res.success) {
-      redirect(`/challenges/${res.data?.id}`);
-    }
-
-    setError("root", { message: res.error.detail });
-  };
+export default async function Page() {
+  await getValidSession();
 
   return (
     <>
       <h1>Create a challenge</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Controller
-          name="name"
-          control={control}
-          render={({ field, fieldState }) => (
-            <Field>
-              <FieldLabel htmlFor={field.name}>Challenge Name</FieldLabel>
-              <Input {...field} id={field.name} placeholder="75 Hard" />
-              {fieldState.error && <FieldError errors={[fieldState.error]} />}
-            </Field>
-          )}
-        />
-        <Controller
-          name="durationDays"
-          control={control}
-          render={({ field, fieldState }) => (
-            <Field>
-              <FieldLabel htmlFor={field.name}>
-                Challenge Duration (days)
-              </FieldLabel>
-              <Input
-                {...field}
-                id={field.name}
-                placeholder="75"
-                type="text"
-                onChange={(e) => {
-                  const digitsOnly = e.target.value.replace(/[^0-9]/g, "");
-                  field.onChange(digitsOnly === "" ? "" : Number(digitsOnly));
-                }}
-              />
-              {fieldState.error && <FieldError errors={[fieldState.error]} />}
-            </Field>
-          )}
-        />
-        <Button type="submit">Create</Button>
-        {formState.errors.root && (
-          <>
-            <p className="text-red-600">
-              Something went wrong: {formState.errors.root?.message}
-            </p>
-            <p className="text-red-600">Try again</p>
-          </>
-        )}
-      </form>
+      <CreateChallengeForm />
     </>
   );
 }
