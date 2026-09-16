f9c0756b0e69ab0c7cc9c87c3de95cebaa02b089
Jack Agtual <jaagtual@gmail.com>
2026-09-15 20:47:15 -0700

    Removing Challenges component

--- web/app/(main)/dashboard/page.tsx
+++ web/app/(main)/dashboard/page.tsx
@@ -1,13 +1,23 @@
+import { client } from "@/lib/api-client";
 import { getValidSession } from "@/lib/auth-utils";
-import Challenges from "./components/Challenges";
+import ChallengeSection from "./components/ChallengeSection";
 
 export default async function Page() {
   await getValidSession();
+  const { data, error } = await client.GET("/challenges");
+
+  if (error) {
+    throw new Error("Could not find your challenges");
+  }
 
   return (
     <>
-      <h1>Dashboard</h1>
-      <Challenges />
+      <h1>Your challenges</h1>
+      <div className="space-y-2">
+        <ChallengeSection title="In Progress" data={data.inProgress} />
+        <ChallengeSection title="Pending" data={data.pending} />
+        <ChallengeSection title="Complete" data={data.complete} />
+      </div>
     </>
   );
 }
